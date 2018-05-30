e2bc027712850f6bb7948f1c51ea7f31d663536f
Mahmuda Akhter <1510930@iub.edu.bd>
2018-05-30 04:17:33 +0000

    classwork_4

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 /* Task 1 */
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 
 
 /* Task 2 using plain JavaScript */
+//task 2a 
+function addText2(){
+	var task2div = document.getElementById('task2a')
+	var paragraph = document.createElement('p')
+	paragraph.innerText = "Hello World"
+	task2div.appendChild(paragraph)
+}
 
+	var textBtn = document.getElementById('textBtn2a')
+	textBtn.addEventListener('click',addText2)
+	
+//Task 2b
+function changeBGcolor(event){
+	var body = document.getElementsByTagName('body')[0]
+	if(event.target.innerText == "Red"){
+		body.style.backgroundColor = 'red'
+	}
+	if(event.target.innerText == "Green"){
+		body.style.backgroundColor = 'green'
+	}		
+}
+//Task 2c
+function redirectToGoogle(){
+	window.location.assign('http://google.com')
 
+}
 
 /* Task 4 using jQuery */
